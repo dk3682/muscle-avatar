de2271092d537fdb4b29a983f8f6a8503c8c5e5f
dk3682 <dk3682@gmail.com>
2026-02-08 01:02:22 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,38 @@
-const CACHE_NAME = "muscle-avatar-v1";
+const CACHE_NAME = 'muscle-avatar-cache-v1';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './assets/avatar/stage-0.svg',
+  './assets/avatar/stage-1.svg',
+  './assets/avatar/stage-2.svg',
+  './assets/avatar/stage-3.svg',
+  './assets/avatar/stage-4.svg',
+  './assets/avatar/stage-5.svg'
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
 });
