71e39df75a1c301b349ec7a461217fedec0cabec
dk3682 <dk3682@gmail.com>
2026-02-07 20:30:10 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+document.documentElement.insertAdjacentHTML(
+  "afterbegin",
+  '<div style="position:fixed;top:50px;left:0;right:0;z-index:999999;background:#00c853;color:#000;padding:10px;font:700 14px -apple-system,system-ui">JS LOADED ✅</div>'
+);
